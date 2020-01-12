91d3eeb6343dc25e5ce45da53d700ab759b41744
Artem Ryzhkov <digital_boy@mail.ru>
2020-01-12 18:23:09 +0700

    add FelibBundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,9 +12,12 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SmartCore\Bundle\DbDumperBundle\SmartDbDumperBundle::class => ['all' => true],
+    SmartCore\Bundle\FelibBundle\FelibBundle::class => ['all' => true],
     SmartCore\Bundle\HtmlBundle\HtmlBundle::class => ['all' => true],
     SmartCore\Bundle\MediaBundle\SmartMediaBundle::class => ['all' => true],
     SmartCore\Bundle\SettingsBundle\SmartSettingsBundle::class => ['all' => true],
     SmartCore\CMSBundle\CMSBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Cache\AdapterBundle\CacheAdapterBundle::class => ['all' => true],
 ];
