8ab1f0469d7b8e54434de12ebdc62074e6d388c6
Artem Ryzhkov <digital_boy@mail.ru>
2020-01-12 18:02:45 +0700

    add SmartMediaBundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,7 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SmartCore\Bundle\DbDumperBundle\SmartDbDumperBundle::class => ['all' => true],
+    SmartCore\Bundle\MediaBundle\SmartMediaBundle::class => ['all' => true],
     SmartCore\CMSBundle\CMSBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
